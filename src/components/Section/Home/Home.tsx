38fa6a5fb7896dad967d78763ada6b191dac7c94
Jaussman Sanchez <jaussmanstefano@gmail.com>
2020-04-14 19:04:00 -0500

    implemented initial information setup and loaded fonts from cdn

--- src/components/Section/Home/Home.tsx
+++ src/components/Section/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.scss";
 import Header from "../../Header";
 import Banner from "../../../shared/components/Banner";
+import Information from "../../../shared/components/Information";
 
 function Home() {
   return (
     <div className="Home">
       <Header />
       <Banner />
+      <Information />
     </div>
   );
 }
---
+++ src/shared/components/Information/Information.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Information.scss";
+
+function Information() {
+  return <div className="Information"></div>;
+}
+
+export default Information;
