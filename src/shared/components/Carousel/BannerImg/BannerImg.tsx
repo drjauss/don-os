f3f0768f2f1b93e08130f7c0c23fb984651a69f8
Jaussman Sanchez <jaussmanstefano@gmail.com>
2020-04-16 20:40:06 -0500

    added 9th image to bio images

--- src/shared/components/Carousel/BannerImg/BannerImg.tsx
+++ src/shared/components/Carousel/BannerImg/BannerImg.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./BannerImg.scss";
 
 function BannerImg() {
-  return <div className="BannerImg">banner img</div>;
+  return (
+    <div className="BannerImg">
+      <div className="center section-padding">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt odit quas porro, harum officiis
+        quidem, rem in aperiam similique expedita eos! Totam, architecto doloribus fugiat inventore provident hic
+        pariatur tempora aliquam nemo reprehenderit aspernatur officia iure consequuntur non quisquam natus voluptates
+        quidem iste tenetur fuga quis! Quia debitis odit quod suscipit, quae esse impedit!
+      </div>
+    </div>
+  );
 }
 
 export default BannerImg;
