9a03f6cca89d707d9b7578a49f80c18ea53354ef
Jaussman Sanchez <jaussmanstefano@gmail.com>
2020-04-16 22:22:44 -0500

    Bography section adjustments and static content set

--- src/shared/components/Carousel/Carousel.tsx
+++ src/shared/components/Carousel/Carousel.tsx
@@ -7,10 +7,18 @@ function Carousel() {
   return (
     <div className="Carousel">
       <div className="section-padding center">
-        <span className="subject">Biografía</span>
-        <h1 className="title with-subject">Oswaldo Díaz Díaz</h1>
-        <BannerImg />
-        <ContentColumns />
+        <div>
+          <span className="subject">Biografía</span>
+          <h1 className="title with-subject no-margin">Oswaldo Díaz Díaz</h1>
+        </div>
+        <div className="section-margin">
+          <BannerImg />
+        </div>
+        <ContentColumns
+          title="La pluma"
+          subtitle="Con la que firmaba y tomaba notas"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur maxime esse voluptas quis autem cumque dignissimos, impedit doloribus nesciunt deleniti."
+        />
       </div>
     </div>
   );
--- src/shared/components/Carousel/ContentColumns/ContentColumns.tsx
+++ src/shared/components/Carousel/ContentColumns/ContentColumns.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./ContentColumns.scss";
 
-function ContentColumns() {
-  return <div className="ContentColumns">Content columns</div>;
+type ContentColumnsProps = {
+  title: string;
+  subtitle: string;
+  content: string;
+};
+
+function ContentColumns({ title, subtitle, content }: ContentColumnsProps) {
+  return (
+    <div className="ContentColumns">
+      <div className="pure-g">
+        <div className="pure-u-1-3">
+          <h2 className="title no-margin left">{title}</h2>
+          <p className="content justified">{subtitle}</p>
+        </div>
+        <div className="pure-u-2-3">
+          <p className="content justified">{content}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContentColumns;
