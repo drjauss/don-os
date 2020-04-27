a487154b4153479b40c211e5cc8d9dbac00f4271
Jaussman Sanchez <jaussmanstefano@gmail.com>
2020-04-27 16:28:30 -0500

    implemented form in subscription component

--- src/shared/components/Subscription/Subscription.tsx
+++ src/shared/components/Subscription/Subscription.tsx
@@ -7,6 +7,16 @@ function Subscription() {
       <div className="center section-padding">
         <h1 className="title white no-margin">Conoce el país de Lilac</h1>
         <p className="content white s1em">Suscríbete a nuestro boletín mensual</p>
+        <form action="mailto:donosvuelvealilac@gmail.com,zumaservicioseditoriales@gmail.com">
+          <input required className="content" type="text" placeholder="Correo electrónico" />
+          <div className="white content">
+            <input required type="checkbox" />
+            Acepto la <a className="underlined">política de privacidad</a>
+          </div>
+          <button className="content" type="submit">
+            Suscribirme
+          </button>
+        </form>
       </div>
     </div>
   );
