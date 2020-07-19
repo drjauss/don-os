c90d8ff407efcfd1d6328d4e5e28a6ad04452b1c
Jaussman Sanchez <jaussmanstefano@gmail.com>
2020-07-18 21:44:22 -0500

    feat: rataplan published at adjustments

---
+++ src/shared/components/Works/definitions/templates/cuento/rataplan/technical-details.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const rataplanPublishedAT = (
+  <span className="white">
+    <i>El país de Lilac</i> (1938), <i>Cuentos tricolores</i> (1967) y <i>Retorno a Lilac</i> (1967)
+  </span>
+);
