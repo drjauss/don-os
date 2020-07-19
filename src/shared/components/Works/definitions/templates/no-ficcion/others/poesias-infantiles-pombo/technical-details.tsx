c4b87432a06a282f930900d5ab1d1f678b63363b
Jaussman Sanchez <jaussmanstefano@gmail.com>
2020-07-18 21:45:42 -0500

    feat: poesias infantiles pombo published at template

---
+++ src/shared/components/Works/definitions/templates/no-ficcion/others/poesias-infantiles-pombo/technical-details.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const poesiasInfantilesPomboPublishedAt = (
+  <span className="white">
+    <i>Hojas de cultura popular colombiana</i>, No. 25. Ministerio de Educación Nacional (1953)
+  </span>
+);
