a9aeac7ff2070e51597ea9b0d5343249d2804f03
Jaussman Sanchez <jaussmanstefano@gmail.com>
2020-07-18 21:44:50 -0500

    feat: como era hans published at template

---
+++ src/shared/components/Works/definitions/templates/no-ficcion/others/como-era-hans/technical-details.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const comoEraHansPublishedAt = (
+  <span className="white">
+    Revista <i>Vida</i>, No. 28. Compañía Colombiana de Seguros (mayo, 1947)
+  </span>
+);
