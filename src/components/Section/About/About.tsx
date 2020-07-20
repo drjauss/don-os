b1218526c21ebb457bfbe323c47d3c6b1cdc96a0
Jaussman Sanchez <jaussmanstefano@gmail.com>
2020-07-19 23:07:43 -0500

    fix: about - agradecimientos ajustes

--- src/components/Section/About/About.tsx
+++ src/components/Section/About/About.tsx
@@ -13,13 +13,13 @@ function About() {
         <SectionTitle title="El proyecto" subtitle="Acerca de" />
         <div className="empty-space"></div>
         <p className="content justified">
-          Este micrositio es el producto final de la investigación titulada <i>Don os vuelve a Lilac</i>: panorama de la
-          obra literaria y radioteatral infantil de Oswaldo Díaz Díaz, ganadora de la “Beca de investigación sobre las
-          colecciones de literatura infantil y juvenil de la Biblioteca Nacional”, obtenida en la Convocatoria
-          Estímulos, Fase II 2019 del Ministerio de Cultura.
+          Este micrositio es el producto final de la investigación titulada{" "}
+          <i>Don os vuelve a Lilac: panorama de la obra literaria y radioteatral infantil de Oswaldo Díaz Díaz</i>,
+          ganadora de la “Beca de investigación sobre las colecciones de literatura infantil y juvenil de la Biblioteca
+          Nacional”, obtenida en la Convocatoria Estímulos, Fase II 2019 del Ministerio de Cultura.
         </p>
         <p className="content justified">
-          Con esta investigación queremos rescatar del olvido, revitalizar y difundir la obra infantil del historiador,
+          Con esta investigación quisimos rescatar del olvido, revitalizar y difundir la obra infantil del historiador,
           escritor y dramaturgo Oswaldo Díaz Díaz, figura destacada en la producción cultural, literaria y radiofónica
           de la primera mitad del siglo XX en Colombia.
         </p>
@@ -60,8 +60,8 @@ function About() {
           >
             Ministerio de Cultura
           </a>
-          &nbsp; por impulsar la investigación en literatura infantil en Colombia y a los miembros del jurado por
-          habernos seleccionado como ganadoras.
+          &nbsp; por impulsar la investigación en literatura infantil en Colombia; a los miembros del jurado por
+          habernos seleccionado como ganadoras y por su voto de confianza en nuestro proyecto.
         </p>
 
         <p className="content justified">
@@ -75,7 +75,7 @@ function About() {
             Biblioteca Nacional
           </a>
           &nbsp; por su apoyo e interés en el desarrollo de la investigación; a Camilo Páez, por su guía y gestión; a
-          Diana Galindo, por su coordinación; a Elder Tovar, por sus lineamientos.
+          Diana Galindo, por su coordinación.
         </p>
 
         <p className="content justified">
@@ -86,14 +86,20 @@ function About() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Señal Memoria
+            Señal Memoria,
           </a>
           &nbsp; por habernos abierto las puertas para conocer y trabajar con su archivo sonoro. A Juan Carlos Murillo,
-          por todo su apoyo y disposición; a Juan David Carvajal, a Germán, a Federico, a Daniel, a Cristian por su gran
-          colaboración en las tareas de búsqueda, restauración y digitalización de los radioteatros de Don Os.
+          por todo su apoyo y disposición; a Juan David Carvajal, a Germán, a Federico, a Daniel y a Cristian por su
+          gran colaboración en las tareas de búsqueda, restauración y digitalización de los radioteatros de Don Os.
         </p>
+
         <p className="content justified">
-          Gracias al &nbsp;
+          Gracias a Carlos Alberto Fernández por su generosidad para compartir con nosotroas su conocimiento del
+          radioteatro en Colombia.
+        </p>
+
+        <p className="content justified">
+          Agradecemos al &nbsp;
           <a
             className="yellow-link no-decoration"
             href="https://gimnasiomoderno.edu.co/"
@@ -106,12 +112,13 @@ function About() {
           habernos permitido acceder a los archivos con los que atamos algunos cabos sueltos.
         </p>
         <p className="content justified">
-          Gracias a Germán Izquierdo por compartirnos el legado de su casiabuelo. Gracias también a Daniela Caicedo, a
-          John Varón y a Gina Navarro por tener un ojo acertado para llevar a Don Os a las imágenes.
+          Gracias a Catalina Díaz, nieta de Oswaldo Díaz Díaz, por abrir la comunicación entre los herederos y la
+          Biblioteca Nacional. También a Germán Izquierdo, por compartir con nosotras el legado de su casiabuelo.
         </p>
+
         <p className="content justified">
-          Gracias a Carlos Alberto Fernández por su generosidad para compartir con nosotras su conocimiento del
-          radioteatro en Colombia.
+          A Fredy Forero todo nuestro agradecimiento por su ayuda incondicional y por esclarecer todas nuestras dudas
+          frente a los derechos de autor de Don Os.
         </p>
         <p className="content justified">
           Gracias a la Pontificia Universidad Javeriana, a la&nbsp;
@@ -123,19 +130,24 @@ function About() {
           >
             Especialización de Literatura Infantil y Juvenil
           </a>
-          &nbsp; y a Jeffrey Cedeño; a&nbsp;
+          &nbsp; y a Jeffrey Cedeño; a la&nbsp;
           <a
             className="yellow-link no-decoration"
-            href="http://fundalectura.org/"
+            href="https://unal.edu.co/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fundalectura
+            Universidad Nacional de Colombia
           </a>
-          , a Diana Rey; a la Universidad Nacional de Colombia, particularmente a Carmen Elisa Acosta y Patricia
-          Trujillo por su voto de confianza en este proyecto desde sus inicios. Gracias por los espacios brindados para
-          divulgar nuestros hallazgos.
+          , particularmente a Carmen Elisa Acosta y Patricia Trujillo por creer en este proyecto desde sus inicios.
+          Gracias por los espacios brindados para divulgar nuestros hallazgos.
         </p>
+
+        <p className="content justified">
+          Gracias también a Daniela Caicedo, a John Varón y a Gina Navarro por tener un ojo acertado para llevar a Don
+          Os a las imágenes y al mundo digital.
+        </p>
+
         <p className="content justified">
           Gracias a nuestras familias y amigos por entender y compartir nuestro amor por Oswaldo Díaz Díaz.
         </p>
